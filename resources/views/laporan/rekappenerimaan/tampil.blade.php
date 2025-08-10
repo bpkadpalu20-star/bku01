2ad0a068f5367723e0f7a62efe239ff64ea7d04e
bpkadpalu20-star <bpkadpalu20@gmail.com>
2025-08-10 13:17:19 +0800

    first commit9.2

--- resources/views/laporan/rekappenerimaan/tampil.blade.php
+++ resources/views/laporan/rekappenerimaan/tampil.blade.php
@@ -42,8 +42,8 @@ tr:hover {
             <img src='{{ url('assets/images/logopalu.png') }}' style="left: 50px" height="100" width="100">
         <img src='repositories/bku01/resources/images/logopalu.png' style="left: 50px" height="100" width="100">
         <img src='../images/logopalu.png' style="left: 50px" height="100" width="100">
-        <img src='{{ URL::asset('../assets/images/logopalu.png')}}' style="left: 50px" height="100" width="100">
-            <img src="{{ URL::asset('assets/images/logo palu.png')}}" style="width: 50px; left: 60px;" alt="" />
+        <img src='{{ URL::asset('images/logopalu.png')}}' style="left: 50px" height="100" width="100">
+            <img src="{{ URL::asset('../images/logopalu.png')}}" style="width: 50px; left: 60px;" alt="" />
         </div>
         <div class="col align-middle fw-bold text-center text-uppercase" style=" margin-top: 15px; text-align: center; font-size: 13px; font-weight: bold;">
             PEMERINTAH KOTA PALU BUKU KAS PENERIMAAN TAHUN ANGGARAN 2025 {{ old('text',$bulan) }}
