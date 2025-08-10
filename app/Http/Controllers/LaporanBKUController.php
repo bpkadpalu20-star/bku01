27e774211a993848ac8bf69043c5907eb517e8d2
bpkadpalu20-star <bpkadpalu20@gmail.com>
2025-08-10 14:07:59 +0800

    first commit9.2

--- app/Http/Controllers/LaporanBKUController.php
+++ app/Http/Controllers/LaporanBKUController.php
@@ -352,6 +352,7 @@ class LaporanBKUController extends Controller implements HasMiddleware
                         'countsts' => $countsts,
                         'countsp2d' => $countsp2d,
                         'bulan' => $bulan,
+                        'Bku' => $Bku,
                 ]);
             }
 
